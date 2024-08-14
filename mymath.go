795a600721e54f80093ace5f3323d7eee9eb78e4
VasIzm <167790047+VasIzm@users.noreply.github.com>
2024-08-14 08:56:16 +0200

    Update mymath.go

--- mymath.go
+++ mymath.go
@@ -26,3 +26,19 @@ func Acosh(x float64) float64 {
 func Asin(x float64) float64 {
 	return math.Asin(x)
 }
+
+func Ceil(x float64) float64 {
+  return math.Ceil(x)
+}
+
+func Floor(x float64) float64 {
+  return math.Floor(x)
+}
+
+func Pow(x, y float64) float64 {
+  return math.Pow(x, y)
+}
+
+func Min(x, y float64) float64 {
+  return math.Min(x, y)
+}
